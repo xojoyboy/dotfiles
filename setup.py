75d9ff840e3a727c2fbb0c84643c316fdf855aea
Harshil C <chudasama.h@northeastern.edu>
2024-01-30 02:33:00 -0800

    Updated setup.py to check for symlink

--- setup.py
+++ setup.py
@@ -8,16 +8,30 @@ def main():
 
     print("Setting up your dotfiles...")
 
-    if system_type == "Windows":
-        # Windows specific commands
-        subprocess.call(['cmd', '/c', 'mklink', '/H', os.path.expanduser('~\\.vimrc'), os.path.join(dotfiles_dir, 'vimrc')])
-        # Use 'mklink /J' for directory junctions (similar to symlinks for directories) in Windows
-    else:
-        # Unix/Linux/MacOS specific commands
-        os.symlink(os.path.join(dotfiles_dir, 'vimrc'), os.path.expanduser('~/.vimrc'), target_is_directory=False)
-        # For directories, use target_is_directory=True
+    create_symlink('vimrc', '.vimrc', dotfiles_dir, system_type)
+    # Add other configuration files as needed
+    # create_symlink('other_config_file', '.other_config_file', dotfiles_dir, system_type)
 
     print("Dotfiles setup completed.")
+
+def create_symlink(source_filename, target_filename, dotfiles_dir, system_type):
+    source_filepath = os.path.join(dotfiles_dir, source_filename)
+    target_filepath = os.path.expanduser(f'~/{target_filename}')
+
+    # If the target file already exists and is a symlink, remove it
+    if os.path.islink(target_filepath):
+        os.unlink(target_filepath)
+    # If it's a file or directory, you might want to back it up instead of removing
+    elif os.path.exists(target_filepath):
+        backup_filepath = f'{target_filepath}.backup'
+        print(f"Backing up existing file: {target_filepath} to {backup_filepath}")
+        os.rename(target_filepath, backup_filepath)
+
+    # Create symlink (Unix) or junction/mklink (Windows)
+    if system_type == "Windows":
+        subprocess.call(['cmd', '/c', 'mklink', '/H', target_filepath, source_filepath])
+    else:
+        os.symlink(source_filepath, target_filepath)
 
 if __name__ == "__main__":
     main()
